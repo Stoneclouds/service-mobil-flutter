0f714850d2a4cc4204a5d269a29a9d7d4abfa0c8
Stoneclouds <clifftonnoya048@gmail.com>
2026-07-16 19:56:38 +0700

    Paket 15 Done

--- android/app/src/main/kotlin/com/example/service_mobil/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.service_mobil
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/constants/api.dart
+++
@@ -1,7 +0,0 @@
-class Api {
-  // Ganti sesuai IP laptop kamu
-  static const String ip = "192.168.18.167";
-
-  // Base URL API
-  static const String baseUrl = "http://$ip/servis_mobile/api";
-}
